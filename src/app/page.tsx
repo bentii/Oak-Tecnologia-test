6b995efb8a697fc72bedcaa59ff6f3eacf33fdbd
bentii <bentivegna666@gmail.com>
2024-06-22 20:52:21 -0300

    oak test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
+"use client";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import Image from "next/image";
+import TrashIcon from "../../public/trash.svg";
+import AddIcon from "../../public/add.svg";
+
+interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  price: number;
+  done: boolean;
+}
 
 export default function Home() {
+  const [taskDetails, setTaskDetails] = useState<Task>({
+    id: Date.now(),
+    title: "",
+    description: "",
+    price: 0,
+    done: false,
+  });
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    setTasks(savedTasks);
+  }, []);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setTaskDetails((prev) => ({
+      ...prev,
+      [name]:
+        type === "checkbox"
+          ? checked
+          : type === "number"
+          ? parseFloat(value)
+          : value,
+    }));
+  };
+
+  const addTask = () => {
+    if (!taskDetails.title) return;
+    const newTask = { ...taskDetails, id: Date.now() };
+    const newTasks = [...tasks, newTask];
+    setTasks(newTasks);
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    setTaskDetails({
+      id: Date.now(),
+      title: "",
+      description: "",
+      price: 0,
+      done: false,
+    });
+  };
+
+  const deleteTask = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  const toggleSort = () => {
+    const direction = sortDirection === "asc" ? "desc" : "asc";
+    setSortDirection(direction);
+    setTasks((tasks) =>
+      [...tasks].sort((a, b) =>
+        direction === "asc" ? a.price - b.price : b.price - a.price
+      )
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col justify-center items-center gap-4 p-2 border-slate-950 border-4">
+      <div className="flex">
+        <h1>Oak App</h1>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex md:flex-row flex-col gap-8 justify-center items-center">
+        <input
+          type="text"
+          name="title"
+          value={taskDetails.title}
+          onChange={handleInputChange}
+          placeholder="Task Title"
         />
+        <input
+          type="text"
+          name="description"
+          value={taskDetails.description || ""}
+          onChange={handleInputChange}
+          placeholder="Task Description"
+        />
+        <input
+          type="number"
+          name="price"
+          value={taskDetails.price}
+          onChange={handleInputChange}
+        />
+        <label>
+          Avaible:
+          <input
+            type="checkbox"
+            name="done"
+            checked={taskDetails.done}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button onClick={addTask}>
+          <Image src={AddIcon} alt="Add" width={20} height={20} />
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <table className="border-slate-950 border-4 w-10/12">
+        <thead className="border-4">
+          <tr>
+            <th className="p-4 text-center border-4">Title</th>
+            <th className="p-4 text-center border-4">Description</th>
+            <th className="p-4 text-center border-4">
+              Price
+              <button onClick={toggleSort}>
+                {sortDirection === "asc" ? "↓" : "↑"}
+              </button>
+            </th>
+            <th className="p-4 text-center border-4">Available</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td className="p-2 text-center border-4">
+                <div className="w-full max-w-xs overflow-auto">
+                  {task.title}
+                </div>
+              </td>
+              <td className="p-2 text-center border-4">
+              <div className="max-w-xs overflow-y-auto overflow-x-hidden">
+                  {task.description}
+                </div>
+              </td>
+              <td className="p-2 text-center border-4">${task.price}</td>
+              <td className="p-2 text-center border-4">{task.done ? "Yes" : "No"}</td>
+              <td className="p-2 text-center border-4">
+                <button onClick={() => deleteTask(task.id)}>
+                  <Image src={TrashIcon} alt="Delete" width={20} height={20} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
